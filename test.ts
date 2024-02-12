0520ccc5cd2ee2c06764947e80fb67a7bc5bc254
barin2010 <uyrirudenko@gmail.com>
2024-02-12 18:45:59 +0200

    empty

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-// function add(num1: number, num2: number): void {
-//   console.log(num1 + num2);
-// }
+function add(num1: number, num2: number): void {
+  console.log(num1 + num2);
+}
 
-// add(1, 4);
+add(1, 4);
